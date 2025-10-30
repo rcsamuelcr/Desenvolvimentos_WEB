d93b8e89ae32916f6468d520de780465c0783fc7
rcsamuelcr <110237797+rcsamuelcr@users.noreply.github.com>
2025-10-30 12:33:20 -0300

    .

--- Aula_Flask/main.py
+++ Aula_Flask/main.py
@@ -10,3 +10,59 @@ def home():
 if __name__ == ('__main__'):
     app.run(debug=True)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     variavel_nome = "Samuel"
+#     is_admin = True
+#     return render_template('index.html', nome=variavel_nome, admin=is_admin)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# <!DOCTYPE html>
+# <html>
+# <head>
+#     <title>Minha Página</title>
+# </head>
+# <body>
+#     <h1>Olá, {{ nome }}!</h1>
+#     {% if admin %}
+#         <p>Você é um administrador.</p>
+#     {% else %}
+#         <p>Você é um usuário comum.</p>
+#     {% endif %}
+# </body>
+# </html>
