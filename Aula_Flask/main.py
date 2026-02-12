04c75220ca8aef0c25080d8d42fd9e4e19a9a80c
rcsamuelcr <110237797+rcsamuelcr@users.noreply.github.com>
2026-02-12 14:35:04 -0300

    Atualização
    s

--- Aula_Flask/main.py
+++ Aula_Flask/main.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
 
+# Configuração do BD
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estudantes.sqlite3'
+
+
+db = SQLAlchemy(app)
+
+# Modelo do Banco
+class Estudante(db.Model):
+    id              = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    primeiro_nome   = db.Column(db.String(150), nullable=False)
+    ultimo_nome     = db.Column(db.String(150), nullable=False)
+    idade           = db.Column(db.Integer, nullable=False)
+
+
+# Crição da tabela dentro do app context
+with app.app_context():
+        db.create_all()         #Todas as tabelas (Estudante)
+
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    estudantes = Estudante.query.all()
+    return render_template('index.html', estudantes=estudantes)
 
 
 if __name__ == ('__main__'):
